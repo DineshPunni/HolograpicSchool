abedfc978b4a49af6414fdd035955194b0c84f2f
Dinesh <dinesh.punni@hotmail.de>
2017-06-06 22:32:37 +0200

    Added cursor and small UI for testing

---
+++ HolographicSchool/Assets/Scripts/AirtapCommand.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AirtapCommand : MonoBehaviour
+{
+	// Called by GazeGestureManager when the user performs a Select gesture
+	void OnSelect()
+	{
+			gameObject.SetActive(false);
+	}
+}
